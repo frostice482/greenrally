336d2bde4f9efde5dab3d80a941f9ccff9fbfe22
Allyri-flora <AllyreSoul@gmail.com>
2025-05-12 22:40:50 +0700

    Added User Feedback for Joining Events

--- index.js
+++ index.js
@@ -16,20 +16,35 @@ for(i = 0; i < itemTitles.length; i++){
 document.getElementById('modal').addEventListener('click', (e) => {
 	e.target.classList.remove('modal');
 })
+document.getElementById('modal-button').addEventListener('click', (e) => {
+	console.log("clicked");
+	const message = document.getElementById('message');
+	console.log(message);
+	message.style.opacity = 1;
+	message.classList.add('message-success');
+	message.innerHTML = '<div class="message-text">You successfully joined the event!</div>';
+	setTimeout(function(){
+		message.style.opacity = 0;
+		setTimeout(function() {
+			message.classList.remove('message-success');
+			message.innerHTML = '';
+		}, 400);
+	}, 3000);
+	})
 
-console.log(itemNames);
+	console.log(itemNames);
 
-function attachPill(item){
-	item.addEventListener('click', (e) => {
-		console.log(e);
-		e.target.classList.toggle('toggled');
-	});
-}
-function attachItem(item){
-	item.addEventListener('click', (e) => {
-		console.log(e);
-		const modal = document.getElementById("modal");
-		modal.classList.add('modal');
-	});
-}
+	function attachPill(item){
+		item.addEventListener('click', (e) => {
+			console.log(e);
+			e.target.classList.toggle('toggled');
+		});
+	}
+	function attachItem(item){
+		item.addEventListener('click', (e) => {
+			console.log(e);
+			const modal = document.getElementById("modal");
+			modal.classList.add('modal');
+		});
+	}
 
