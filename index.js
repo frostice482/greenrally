1f8e2e55a781ed46443f49e47aa2d5d9e59f08ad
Allyri-flora <AllyreSoul@gmail.com>
2025-05-12 20:13:55 +0700

    Added Base Modal Implementation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let pills = document.getElementsByClassName('pill');
 let items = document.getElementsByClassName('item');
+let itemTitles = document.getElementsByClassName('item-title')
+let itemNames = [];
 
 for(i = 0; i < pills.length; i++){
 	attachPill(pills.item(i));
@@ -7,17 +9,24 @@ for(i = 0; i < pills.length; i++){
 for(i = 0; i < items.length; i++){
 	attachItem(items.item(i));
 }
+for(i = 0; i < itemTitles.length; i++){
+	itemNames.push(itemTitles.item(i).innerHTML);
+}
+
+
+console.log(itemNames);
 
 function attachPill(item){
 	item.addEventListener('click', (e) => {
 		console.log(e);
 		e.target.classList.toggle('toggled');
-
 	});
 }
 function attachItem(item){
 	item.addEventListener('click', (e) => {
 		console.log(e);
-
+		const modal = document.getElementById("modal");
+		modal.classList.add('modal');
 	});
 }
+
